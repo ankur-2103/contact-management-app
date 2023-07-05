0da36c08c0ed69b43147f13c2744e620d8a202b2
ankur-2103 <85174725+ankur-2103@users.noreply.github.com>
2023-07-05 21:25:10 +0530

    fix style

--- src/Pages/NotFound.tsx
+++ src/Pages/NotFound.tsx
@@ -8,9 +8,9 @@ const NotFound = () => {
 
     // Returns not found page
     return (
-        <div className='flex flex-col items-center justify-center flex-1'>
+        <div className='flex flex-col items-center justify-center flex-1 gap-4'>
             <span>Page Not Found!</span>
-            <span className='flex gap-2'>Please go back to <NavLink to={'/contact'} className='text-blue-500 font-bold'>Contact</NavLink>or<NavLink to={'/maps-and-charts'} className='text-blue-500 font-bold'>Maps and Charts</NavLink></span>
+            <span className= 'flex flex-col items-center md:flex-row md:gap-2'>Please go back to <NavLink to={'/contact'} className='text-blue-500 font-bold'>Contact</NavLink> or <NavLink to={'/maps-and-charts'} className='text-blue-500 font-bold'>Maps and Charts</NavLink></span>
         </div>
     )
 }
